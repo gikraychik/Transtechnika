2d0af2a674287296fa90d3f804555a15bd23c3ea
George Kraychik <gkraychik@localhost.localdomain>
2014-03-14 03:45:21 -0400

    Fixed bug

--- npptransteh.com/www/view.php
+++ npptransteh.com/www/view.php
@@ -4,8 +4,15 @@
 	<title>Фотографии</title>
 </head>
 <body>
-	<img src="images/new/tr1.jpg", width="33%">
-	<img src="images/new/tr2.jpg", width="33%">
-	<img src="images/new/tr2.jpg", width="33%">
+	<!--<img src="images/new/tr1.jpg", width="33%" height = "500">
+	<img src="images/new/tr2.jpg", width="33%" height = "500">
+	<img src="images/new/tr3.jpg", width="33%" height = "500"> -->
+	<table width="100%" border="0">
+	<tr>
+		<td width="30%" align="center"><a href="images/new/tr1.jpg"><img src="images/new/tr1.jpg" width="100%"></a></td>
+		<td align="center" valign="center" width = "40%"><a href="images/new/tr3.jpg"><img src="images/new/tr3.jpg" width="100%"></a></td>
+		<td width="30%" align="center"><a href="images/new/tr2.jpg"><img src="images/new/tr2.jpg" width="100%"></a></td>
+	</tr>
+	</table>
 </body>
 </html>
